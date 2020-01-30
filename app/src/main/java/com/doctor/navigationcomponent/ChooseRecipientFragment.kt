c6bc63dc6447a9acac650715b74e60711328d167
Humam <jakhumam@gmail.com>
2020-01-30 18:10:30 +0700

    Initial commit

---
+++ app/src/main/java/com/doctor/navigationcomponent/ChooseRecipientFragment.kt
@@ -0,0 +1,58 @@
+package com.doctor.navigationcomponent
+
+
+import android.os.Bundle
+import android.os.IBinder
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_choose_recipient.*
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
+
+class ChooseRecipientFragment : Fragment(),View.OnClickListener {
+    lateinit var navController: NavController
+//    lateinit var recipient: String
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        recipient = arguments!!.getString("recipient")
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_recipient, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.next_btn -> {
+                if (!TextUtils.isEmpty(input_recipient.text.toString())){
+                    val bundle = bundleOf("recipient" to input_recipient.text.toString())
+                    navController!!.navigate(
+                        R.id.action_chooseRecipientFragment_to_specifyAmountFragment,
+                        bundle)
+                }else{
+                    Toast.makeText(activity, "Enter a recipient", Toast.LENGTH_LONG).show()
+            }
+        }
+            R.id.cancel_btn -> activity!!.onBackPressed()
+    }
+  }
+}
